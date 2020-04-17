354b7ae1119eb3391e78351f583ae8acdb8c4422
elena <papakos21@gmail.com>
2020-04-17 22:37:19 +0300

    allaksa noumera

--- functions_test.py
+++ functions_test.py
@@ -1,19 +1,12 @@
 from functions import get_dekada
 
 def test_dekada():
-    assert get_dekada()[0] == 1
+    assert get_dekada()[1] == 2
 
 
 
 
-# Some more tests to show how to name tests (naming convention )
 
-
-def test_get_dekada_first_number_should_be_one():
-    assert get_dekada()[0] == 1
-
-def test_get_dekada_last_number_should_be_ten():
-    assert get_dekada()[-1] == 10
 
 
 
