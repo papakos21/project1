30704f48724a0fa185f98ed8ffedc8812ef405bc
elena <papakos21@gmail.com>
2020-04-29 00:19:59 +0300

    implemented black or white

--- colors.py
+++ colors.py
@@ -1,17 +1,18 @@
+def black_or_white(input_number):
 
-def black_or_white(input):
-    pass
+    if input_number == 0:
+        return "white"
+    if input_number == 1:
+        return "black"
 
 
 def my_colors(input):
-
-    #use if else control flow
+    # use if else control flow
     pass
-
 
 
 def my_colors_map(input):
     pass
 
-    #create a dictionary color_dic keys are a numbers and values are color names 'red', 'green' , etc
+    # create a dictionary color_dic keys are a numbers and values are color names 'red', 'green' , etc
     # return color_dict[input]
