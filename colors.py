a253e98b34186bb5ac0467191bafdca24ba426b8
elena <papakos21@gmail.com>
2020-04-29 00:35:22 +0300

    implemented 4 colors

--- colors.py
+++ colors.py
@@ -1,14 +1,23 @@
 def black_or_white(input_number):
-
     if input_number == 0:
         return "white"
     if input_number == 1:
         return "black"
 
 
-def my_colors(input):
+# 0-->white, 1-->black, 2-->blue, 3-->green
+def my_colors(input_number):
     # use if else control flow
-    pass
+    if input_number == 0:
+        return "white"
+    elif input_number == 1:
+        return "black"
+    elif input_number == 2:
+        return "blue"
+    elif input_number == 3:
+        return "green"
+    else :
+        return "unknown"
 
 
 def my_colors_map(input):
