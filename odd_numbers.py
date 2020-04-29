401c96af8f798570b3281484cb8c032557a6677a
elena <papakos21@gmail.com>
2020-04-30 01:50:09 +0300

    put table inside function , problem solved

--- odd_numbers.py
+++ odd_numbers.py
@@ -1,14 +1,15 @@
-table = []
+
 
 
 def all_odd(num):
+    table=[]
     num = int(num)
     while num > 1:
         if num % 2 == 0:
 
             for x in range(1, num, 2):
                 table.append(x)
-                return table
+            return table
         else:
             num = num + 1
             for x in range(1, num, 2):
--- odd_numbers_test.py
+++ odd_numbers_test.py
@@ -1,5 +1,18 @@
 from odd_numbers import all_odd
 
-def test_odd_numbers():
-    assert all_odd(5) == [1, 3, 5]
 
+def test_odd_numbers_2():
+    assert all_odd(2) == [1]
+
+
+def test_odd_numbers_3():
+    assert all_odd(3) == [1, 3]
+
+
+def test_odd_numbers_10():
+    assert all_odd(10) == [1, 3, 5, 7, 9]
+
+# all_odd(2) returns [1]
+# all_odd(3) returns [1,3]
+
+# all_odd(10) returns [1,3,5,7,9]
