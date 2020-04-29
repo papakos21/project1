eb8d630593f16bd0bfd1c08f57aeb41453ed15d8
elena <papakos21@gmail.com>
2020-04-29 21:21:23 +0300

    millionaire

--- askisi1_epitokio_avouris.py
+++ askisi1_epitokio_avouris.py
@@ -1,16 +1,19 @@
 # πρόγραμμα που με βάση τα χρήματα και το επιτόκιο σου λέει σε πόσα χρόνια θα εισαι millionaire
+def years_million():
+    years = 0
+    money = input("Πόσα λεφτά έχεις; :")
+    money = int(money)
+    epitokio = input("Mε πόσο ετήσιο επιτόκιο %; :")
+    epitokio = float(epitokio)
+
+    while money < 1000000:
+        Akrives_epitokio = (epitokio / 100) * money
+        Akrives_epitokio = float(Akrives_epitokio)
+        money = money + Akrives_epitokio
+        years = years + 1
+    else:
+        print("σε {} χρόνια θα είσαι millionaire!!".format(years))
+        return years
 
 
-years = 0
-money = input("Πόσα λεφτά έχεις; :")
-money = int(money)
-epitokio = input("Mε πόσο ετήσιο επιτόκιο %; :")
-epitokio = float(epitokio)
-
-while money < 1000000:
-    Akrives_epitokio = (epitokio / 100) * money
-    Akrives_epitokio = float(Akrives_epitokio)
-    money = money + Akrives_epitokio
-    years = years + 1
-else:
-    print("σε {} χρόνια θα είσαι millionaire!!".format(years))
+years_million()
