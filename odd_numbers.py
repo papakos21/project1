d6df4a48d366caa832504d12e6a66fbbd22d6f21
elena <papakos21@gmail.com>
2020-04-30 11:16:14 +0300

    passed all tests

--- odd_numbers.py
+++ odd_numbers.py
@@ -1,20 +1,20 @@
-
-
-
 def all_odd(num):
     table=[]
     num = int(num)
-    while num > 1:
-        if num % 2 == 0:
+    while num > 0:
+         if num % 2 == 0:
 
             for x in range(1, num, 2):
                 table.append(x)
             return table
-        else:
+         elif num%2 != 0:
             num = num + 1
             for x in range(1, num, 2):
                 table.append(x)
+
             return table
+    else :
+        return table
 
 
 
