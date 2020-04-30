0c433d60ef4d88b1c2cd5101b3b385534bece152
Karolos <karolos.kantounis@gmail.com>
2020-04-30 13:19:40 -0400

    separated business and view logic , broke business logic into smaller functions ( single responsibility principle )

--- askisi2.py
+++ askisi2.py
@@ -1,22 +1,37 @@
-def askisi_2():
+# my_sum in order not to confuse with built in python function sum
+def my_sum(x,y):
+    return x+y
 
-    lala  = True
-    while  lala:
+
+def my_product(x,y):
+    return x*y
+
+
+# a function should ideally do only one thing so breaking it to two
+def sum_and_product(x,y,):
+    return my_sum(x, y), my_product(x, y)
+
+
+# the view function ( a bit difficult but not impossible to write tests - do not write tests for now)
+def sum_and_product_interactive():
+    while True:
         x = int(input("δώσε x:"))
         y = int(input("δώσε y:"))
-        athr = x + y
-        gino = x * y
+
+        # business logic moved to other function
+        (athr, gino) = sum_and_product(x,y)
+
         print("To άθροισμα είναι {}".format(athr))
         print("Τό γινόμενο είναι {}".format(gino))
 
         apantisi = input("να συνεχίσω;")
+
         if apantisi == "stop":
             break
-        else :
+        else:
             continue
 
-        #return (athr, gino)
-askisi_2()
+
 
 #programma pou zitaei duo arithmous k dinei to athroisma kai to ginomeno , se rwtaei na sunexisei k stamataei
 #an dwseis apantisi stop alliws sunexizei. epeidi to ekana function k afto exw kolllisei me to return gia na mporesw
