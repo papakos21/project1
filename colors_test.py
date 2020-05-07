3db82e90908025d2b26f25b01fbe10f740489cfa
elena <papakos21@gmail.com>
2020-05-08 00:41:40 +0300

    colors and functions back

---
+++ colors_test.py
@@ -0,0 +1,32 @@
+import colors
+
+
+def test_black_or_white_returns_white_for_0():
+    assert colors.black_or_white(0) == 'white'
+
+
+def test_black_or_white_returns_black_for_1():
+    assert colors.black_or_white(1) == 'black'
+
+
+def test_my_colors_returns_blue_for_1():
+    assert colors.my_colors(1) == 'black'
+
+
+
+
+def test_my_colors_returns_blue_for_2():
+    assert colors.my_colors(2) == 'blue'
+
+def test_my_colors_returns_unknown_for_all_the_rest():
+    assert colors.my_colors(10) == 'unknown'
+
+def test_my_colors_map_returns_blue_for_2():
+    assert colors.my_colors_map(2) == 'blue'
+
+
+def test_my_colors_map_returns_blue_for_1():
+        assert colors.my_colors_map(1) == 'black'
+
+def test_my_colors_map_returns_unknown_for_all_the_rest():
+    assert colors.my_colors_map("elena") == 'unknown'
---
+++ functions.py
@@ -0,0 +1,7 @@
+def get_dekada():
+    result = []
+
+    for x in range(1, 11):
+           result.append(x)
+
+    return result
