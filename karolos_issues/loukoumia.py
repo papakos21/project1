f062179ab590b47f14aaba69aae8659d8a0f71e6
elena <papakos21@gmail.com>
2020-05-09 15:58:29 +0300

    finished

--- karolos_issues/loukoumia.py
+++ karolos_issues/loukoumia.py
@@ -1,10 +1,24 @@
-import numpy as np
-def get_word(symvoloseira):
+def get_word(input_word):
+    input_word = str(input_word)
+    list1 = []
+    list2 = []
+    list3 = []
+    if len(input_word) <= 9:
+        input_word = input_word.ljust(9," ")
 
-    lista = (list(symvoloseira))
-    print (np.split(lista,[2,6]))
+        for i in range(len(input_word)):
+            if i <= 2:
+                list1.append(input_word[i])
+            elif 2 < i <= 5:
+                list2.append(input_word[i])
+            elif 5 < i <= 8:
+                list3.append(input_word[i])
+        print(list1, list2, list3)
 
-get_word('loukoumia')
+    elif len(input_word) > 9:
+        print("too long!!!!")
 
 
 
+
+get_word("elena")
