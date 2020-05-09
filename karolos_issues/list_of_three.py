5de41b671ac1b0182570d6c2690dc5c94d79bd1c
elena <papakos21@gmail.com>
2020-05-09 22:17:52 +0300

    closes #10 using YOUR IDEAS KAROLE

--- karolos_issues/list_of_three.py
+++ karolos_issues/list_of_three.py
@@ -1,24 +1,19 @@
+def split(word):
+    return list(word)
+
 def get_word(input_word):
+    listara=[]
     input_word = str(input_word)
-    list1 = []
-    list2 = []
-    list3 = []
-    list4 = []
     if len(input_word) <= 9:
+        n = 3
         input_word = input_word.ljust(9, " ")
-
-        for i in range(len(input_word)):
-            if i <= 2:
-                list1.append(input_word[i])
-            elif 2 < i <= 5:
-                list2.append(input_word[i])
-            elif 5 < i <= 8:
-                list3.append(input_word[i])
-        list4 = [list1, list2, list3]
-        return list4
-
-    elif len(input_word) > 9:
+        listoula = [input_word[i:i + n] for i in range(0, len(input_word), n)]
+        for i in range (len(listoula)):
+            listara.append(split(listoula[i]))
+        return listara
+    elif len(input_word) >=9 :
         return "too long!!!!"
 
 
-get_word('abcdefghi')
+get_word("abcdefghi")
+
